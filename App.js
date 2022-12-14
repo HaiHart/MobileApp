b8febaabb8a8f6558c6d9ab55bad1c5628bf5745
quocthanhh <dangthanh3819@gmail.com>
2022-12-14 17:08:55 +0700

    Services

--- App.js
+++ App.js
@@ -1,41 +1,48 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from "./Components/LogIn";
-import Home from "./Components/Home";
-import Register from "./Components/Register";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Plus from "./assets/Plus.png";
+import ServiceRenter from "./pages/ServiceRenter";
+import ServiceManager from "./pages/ServiceManager";
 
-const Stack = createNativeStackNavigator();
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Login"
-        component = {Login}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="Register"
-        component = {Register}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="Home"
-        component = {Home}
-        options={{headerShown: false}}
-      />
-    </Stack.Navigator>
-   </NavigationContainer>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name === "Service Renter") {
+              return <Image source={Plus}></Image>;
+            } else if (route.name === "Service Mangager") {
+              return <Image source={Plus}></Image>;
+            }
+          },
+        })}
+      >
+        <Tab.Screen
+          name="Service Renter"
+          component={ServiceRenter}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="Service Mangager"
+          component={ServiceManager}
+        ></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    display: "flex",
     justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
   },
 });
+
+export default App;
