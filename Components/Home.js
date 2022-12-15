73f7f77087d596ca7032c8165a446f434cd138a7
vnminhhuy2001 <huyvnm@fpt.com.vn>
2022-12-15 07:23:26 +0700

    Add navigator to Profile screen at Home.js

--- Components/Home.js
+++ Components/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View,Text, Button} from "react-native";
 
 export default function Home({navigation, route}) {
-    console.log(">> Home screen")
+    
+    
     return(
         <View>
             <Text>
                 {route.params.info} Home
             </Text>
+            <Button onPress={() => navigation.navigate("Profile")}
+                title="Profile"
+            />
         </View>
     )
 }
